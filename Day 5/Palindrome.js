6e2d168c52a4c34327752a2f6bd7cddbd33d6aa1
munyenze1998 <josephmunyenze@gmail.com>
2024-01-09 17:26:03 +0300

    Testing For Palindrome Numbers
    Used JS Functions

--- Day 5/Palindrome.js
+++ Day 5/Palindrome.js
@@ -6,7 +6,6 @@ function isPalindrome(number) {
   // Test the function
   const num1 = 121;
   const num2 = 12345;
-  
   console.log(`${num1} is a palindrome: ${isPalindrome(num1)}`); // true
   console.log(`${num2} is a palindrome: ${isPalindrome(num2)}`); // false
   
