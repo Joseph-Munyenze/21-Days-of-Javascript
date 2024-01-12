9c17f8323765eef8c562e00dd69f10b358f58316
munyenze1998 <josephmunyenze@gmail.com>
2024-01-12 18:49:41 +0300

    jQuery basics

---
+++ Day 8/main.js
@@ -0,0 +1,15 @@
+
+// $(document).ready(function()){
+//     $('button').click(function(event){
+//         event.preventDefault();
+//         let word=$('#palindrome-input').val().toLowerCase();
+//         let isPalindrome=checkPalindrome(word)
+// })
+// })
+
+// function checkPalindrome(word){
+//     let len=word.length;
+//     for(let i=0;i<len/2;i++){
+//         if(word[i])
+//     }
+// }
